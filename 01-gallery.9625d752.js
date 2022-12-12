cda8056f20605e7c094e0edc3fb993843dffd96c
ADIKryvonos <108137330+ADIKryvonos@users.noreply.github.com>
2022-12-12 13:44:57 +0000

    Deploying to gh-pages from @ ADIKryvonos/goit-js-hw-08@9c0fa0c41421f865228abb3185cbda0f4fb05e19 🚀

--- 01-gallery.9625d752.js
+++
@@ -1,2 +0,0 @@
-!function(){var a=document.querySelector(".gallery"),e=galleryItems.map((function(a){var e=a.preview,n=a.original,t=a.description;return'<a class="gallery__item" href="'.concat(n,'">\n  <img class="gallery__image" src="').concat(e,'" alt="').concat(t,'" />\n</a>')})).join("");a.insertAdjacentHTML("beforeend",e);new SimpleLightbox(".gallery a",{captionDelay:250,captionsData:"alt"})}();
-//# sourceMappingURL=01-gallery.9625d752.js.map
